0774ca166ddeae9b829eeb0ca3013ad7e7e5fcd2
Keith Ong <keith.oys@gmail.com>
2018-11-13 11:36:33 +0800

    - Added app routing outlet - Added all pages' components - Added global stylesheet scss

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,28 +1,45 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpModule } from '@angular/http';
+import { MDBBootstrapModule } from 'angular-bootstrap-md';
 import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterSuccessComponent } from './register-success/register-success.component';
+import { LoginSuccessComponent } from './login-success/login-success.component';
 import { ServerService } from './server.service';
 
-import { MDBBootstrapModule } from 'angular-bootstrap-md';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+const appRoutes: Routes = [
+    { path: '', redirectTo: '/login', pathMatch: 'full' },
+    { path: 'register', component: RegisterComponent },
+    { path: 'login', component: LoginComponent },
+    { path: 'register-success', component: RegisterSuccessComponent },
+    { path: 'login-success', component: LoginSuccessComponent },
+];
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    BrowserAnimationsModule,
-    HttpModule,
-    MDBBootstrapModule.forRoot(),
-    FormsModule,
-    ReactiveFormsModule
-  ],
-  providers: [ServerService],
-  bootstrap: [AppComponent],
-  schemas: [ NO_ERRORS_SCHEMA ]
+    declarations: [
+        AppComponent,
+        RegisterComponent,
+        LoginComponent,
+        RegisterSuccessComponent,
+        LoginSuccessComponent
+    ],
+    imports: [
+        BrowserModule,
+        BrowserAnimationsModule,
+        HttpModule,
+        MDBBootstrapModule.forRoot(),
+        FormsModule,
+        ReactiveFormsModule,
+        RouterModule.forRoot(appRoutes)
+    ],
+    providers: [ServerService],
+    bootstrap: [AppComponent],
+    schemas: [NO_ERRORS_SCHEMA]
 })
 export class AppModule { }
